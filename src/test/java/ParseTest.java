efbd33c88e84924cb1ec1251812bc1fc2c5802ce
Tak <env.tak@gmail.com>
2019-10-14 21:15:47 +0900

    test: add simple test case

--- src/test/java/ParseTest.java
+++ src/test/java/ParseTest.java
@@ -1,2 +1,14 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
 public class ParseTest {
+
+    @Test
+    public void runSimpleTest() {
+        byte[] expected = "trial".getBytes();
+        byte[] actual = "trial".getBytes();
+        assertArrayEquals("failure - byte arrays not same", expected, actual);
+    }
+
 }
