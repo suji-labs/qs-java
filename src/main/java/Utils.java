f03670d64ceaa7b3d0903addea646ccc77214456
ledgku <ledgku@gmail.com>
2019-10-16 21:00:25 +0900

    feat: utils combine

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils {
+    public static <T> List<T> combine(T a, T b) {
+        List<T> result = new ArrayList<>();
+
+        result.add(a);
+        result.add(b);
+
+        return result;
+    }
+
+    public static <T> List<T> combine(List<T> a, T b) {
+        List<T> result = new ArrayList<>();
+
+        result.addAll(a);
+        result.add(b);
+
+        return result;
+    }
+
+    public static <T> List<T> combine(T a, List<T> b) {
+        List<T> result = new ArrayList<>();
+
+        result.add(a);
+        result.addAll(b);
+
+        return result;
+    }
+
+    public static <T> List<T> combine(List<T> a, List<T> b) {
+        List<T> result = new ArrayList<>();
+
+        result.addAll(a);
+        result.addAll(b);
+
+        return result;
+    }
 }
--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -1,2 +1,27 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class UtilsTest {
+    @Test
+    public void testCombine() {
+        Utils utils = new Utils();
+
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        List<Integer> combined = new ArrayList<>();
+
+        a.add(1);
+        b.add(2);
+        combined.add(1);
+        combined.add(2);
+
+        assertEquals("both arrays", combined, utils.combine(a, b));
+        assertEquals("one array, one non-array", combined, utils.combine(a, 2));
+        assertEquals("one non-array, one array", combined, utils.combine(1,  b));
+        assertEquals("neither is an array", combined, utils.combine(1,  2));
+    }
 }
